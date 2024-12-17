eb31ffc2a010d5a37ce426455edfc09951c6b1df
Richmond Zedrik Aspacio <richmondaspacio.ra@gmail.com>
2024-12-18 02:38:36 +0800

    Initial commit

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ const wss = new WebSocket.Server({ server });
 
 const corsOptions = {
     origin: process.env.NODE_ENV === 'production'
-      ? ['https://disasterapp.vercel.app./']
+      ? ['https://disasterapp-u794h5q0t-richmondzedriks-projects.vercel.app', 'https://disasterapp.vercel.app']
       : ['http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
